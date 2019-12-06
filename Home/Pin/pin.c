1a6b953c9bace1484cc16f01e9f6d6b9c11a1226
das.luca <37372149+DasLuca@users.noreply.github.com>
2019-12-06 18:45:45 +0100

    Update

---
+++ Home/Pin/pin.c
@@ -0,0 +1,37 @@
+// Autor: Luca Gaerisch
+// Pin
+
+#include <stdio.h>
+
+int main(void)
+{ // Start main
+    int pin[4] = { 1,2,3,4 };
+	int abfrage[4];
+	int test=0;
+
+	for(int i=0; i<4; i++)
+	{
+		printf("Bitte geben Sie die %i Ziffer ein: ", i+1);
+		scanf("%i", &abfrage[i]);
+	}
+
+	for(int i=0; i<4; i++)
+	{
+		if(pin[i] == abfrage[i])
+		{
+			test=test+1;
+		}
+		else
+		{
+			printf("NOPE!");
+			return 0;
+		}
+	}
+
+    if(test==4)
+    {
+    	printf("YESSS");
+    }
+
+    return 0;
+} // End main
