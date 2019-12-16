8167cad57427520dec5fbac928280063b547435e
das.luca <37372149+DasLuca@users.noreply.github.com>
2019-12-16 19:31:46 +0100

    ...

--- Home/Calculator/calculator.c
+++ Home/Calculator/calculator.c
@@ -9,6 +9,9 @@ float addition();
 float subtraction();
 float multi();
 float division();
+float quadrat();
+float potenz();
+float quer();
 
 int main(void)
 { // Start main
@@ -16,14 +19,14 @@ int main(void)
 
     clear();
     printf("========== CALCULATOR ==========\n");
-    printf("1) Addition\t2) Subtraction\n3) Mutli\t4) Division\n5) Exit\n");
+    printf("1) Addition\t2) Subtraction\n3) Mutli\t4) Division\n5) Quadrat\t6) Potenz\n7) Queer\t8) Exit\n");
 
     do
     {
         printf("================================\n");
         printf("Selection: ");
         scanf("%i", &choice);
-    } while (choice<=0 || choice>=6);
+    } while (choice<=0 || choice>=9);
 
     switch(choice)
     {
@@ -31,7 +34,10 @@ int main(void)
         case 2: subtraction(); break;
         case 3: multi(); break;
         case 4: division(); break;
-        case 5: return 0; break;
+        case 5: quadrat(); break;
+        case 6: potenz(); break;
+        case 7: quer(); break;
+        case 8: return 0; break;
     }
 
     return 0;
@@ -95,12 +101,60 @@ float division()
     float num1,num2,res;
 
     clear();
-    printf("========== Division ==========\n");
+    printf("========== DIVISION ==========\n");
     printf("First number: ");
     scanf("%f", &num1);
     printf("Second number: ");
     scanf("%f", &num2);
     res = num1 / num2;
+    printf("Result: %g\n", res);
+    system("pause");
+    main();
+}
+
+float quadrat()
+{
+    float num1,num2,res;
+
+    clear();
+    printf("========== QUADRAT ==========\n");
+    printf("Number: ");
+    scanf("%f", &num1);
+    res = num1*num1;
+    printf("Result: %g\n", res);
+    system("pause");
+    main();
+}
+
+float potenz()
+{
+    float num1,num2,res;
+
+    clear();
+    printf("========== POTENZ ==========\n");
+    printf("First number: ");
+    scanf("%f", &num1);
+    printf("Second number: ");
+    scanf("%f", &num2);
+
+    for(int i=1; i<num2; i++)
+    {
+        res=num1*num2;
+    }
+
+    printf("Result: %g\n", res);
+    system("pause");
+    main();
+}
+
+float quer()
+{
+    float num1,num2,res;
+
+    clear();
+    printf("========== QUER ==========\n");
+    printf("First number: ");
+    scanf("%f", &num1);
     printf("Result: %g\n", res);
     system("pause");
     main();
---
+++ School/Functions/diskriminante.c
@@ -0,0 +1,56 @@
+// Autor: Luca Gaerisch
+// Diskriminante
+
+#include <stdio.h>
+#include <math.h>
+
+float diskriminante(float x, float y, float z);
+float x1(float x, float y, float z);
+
+int main(void)
+{ // Start main
+    float a,b,c,dis,y;
+
+    printf("Bitte geben Sie den punkt a an: ");
+    scanf("%f", &a);
+    printf("Bitte geben Sie den punkt b an: ");
+    scanf("%f", &b);
+    printf("Bitte geben Sie den punkt c an: ");
+    scanf("%f", &c);
+
+    printf("X\t|\tY\n");
+    for(int x=-5; x<=5; x++)
+    {
+        y=(a * (x * x) + (b * x) + c);
+        printf("%i\t|\t%g\n", x, y);
+    }
+
+    dis = diskriminante(a,b,c);
+
+    if(dis==0)
+    {
+        printf("Die Nullstelle ist: ( %g | 0 )\n", x1(a,b,dis));
+        
+    }
+    else if(dis<0)
+    {
+        printf("Es gibt keine reelle loesung fuer die Nullstelle!\n");
+
+    }
+    else
+    {
+        printf("Die 1. Nullstelle ist: ( %g | 0 )\n", x1(a,b,dis));
+        printf("Die 2. Nullstelle ist: ( %g | 0 )\n", x1(a,b,dis));
+    }
+    return 0;
+} // End main
+
+float diskriminante(float x, float y, float z)
+{
+    return (y * y) - 4 * x * z;
+}
+
+float x1(float x, float y, float z)
+{
+    return ((-1 * y) + sqrt(z)) / (2 * x); // sqrt == Wurzel aus
+}
